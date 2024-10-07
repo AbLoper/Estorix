a188b666feb192e253b46afb5c340fe665c0a6df
AbLoper <abedslayman1982@gmail.com>
2024-10-07 15:52:05 +0300

    Last Update 7/10/2024

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './styles/cssModules/App.module.css'
 
 function App() {
   return (
-    <h1>Estorix Web Application</h1>
+    <>
+      <h1>Estorix Web Application</h1>
+      <h3>Last Update 7/10/2024</h3>
+    </>
   )
 }
 export default App
