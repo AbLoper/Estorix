21676b31f5949016dec3a4eaa6d1aa0c77061f70
AbLoper <abedslayman1982@gmail.com>
2025-01-16 16:33:33 +0200

    Update Code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,115 +1,23 @@
-import {
-    MDBContainer,
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarToggler,
-    MDBIcon,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBBtn,
-    MDBCollapse,
-    MDBBadge,
-} from 'mdb-react-ui-kit';
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-// import Styles from '../css/navbar.module.css'
-import logo from '../../assets/images/logos/shopping-cart-logo.png';
+import NavbarSM from './NavbarSM';
+import NavbarLG from './NavbarLG';
 
-export default function NavbarApp() {
-    const [openBasic, setOpenBasic] = useState(false);
-    const cartItemsLength = useSelector(state => state.cart.items.length);
-    const location = useLocation();
-    const [userWelcomeStyle, setUserWelcomeStyle] = useState({});
-    const [userVisitStyle, setUserVisitStyle] = useState({});
-    const [userCartStyle, setUserCartStyle] = useState({});
-    const [cartItemsCount, setCartItemsCount] = useState(cartItemsLength)
+const NavbarApp = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
-        setCartItemsCount(cartItemsLength)
-    }, [cartItemsLength])
+        const checkScreenSize = () => {
+            setIsSmallScreen(window.innerWidth <= 992); 
+        };
 
-    useEffect(() => {
-        // console.log('Current location:', location.pathname);
+        checkScreenSize();
 
-        if (location.pathname === '/login' || location.pathname === '/register') {
-            setUserWelcomeStyle({ display: 'none' });
-            setUserVisitStyle({ display: 'none' });
-            setUserCartStyle({ display: 'none' });
-        } else {
-            setUserWelcomeStyle({ display: 'flex' });
-            setUserVisitStyle({
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                // gap: '.5rem',
-            });
-            setUserCartStyle({ display: 'flex' });
-        }
-    }, [location]);
+        window.addEventListener('resize', checkScreenSize);
 
-    return (
-        <MDBNavbar expand='md' light bgColor='light' fixed='top'>
-            <MDBContainer>
-                <MDBNavbarBrand href='/' >
-                    <img
-                        src={logo}
-                        height='30'
-                        alt='logo'
-                        loading='lazy'
-                    />
-                </MDBNavbarBrand>
+        return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
 
-                <MDBNavbarToggler
-                    aria-controls='navbarSupportedContent'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                    onClick={() => setOpenBasic(!openBasic)}
-                >
-                    <MDBIcon icon='bars' fas />
-                </MDBNavbarToggler>
+    return isSmallScreen ? <NavbarSM /> : <NavbarLG />;
+};
 
-                <MDBCollapse navbar open={openBasic}>
-                    {/* <div className={Styles.collapse}> */}
-                    <div className='d-md-flex align-items-center'>
-                        <div>
-                            <MDBNavbarNav>
-                                <MDBNavbarItem>
-                                    <MDBNavbarLink active href="/products">Products</MDBNavbarLink>
-                                </MDBNavbarItem>
-                                <MDBNavbarItem>
-                                    <MDBNavbarLink href="/contactus">Contact Us</MDBNavbarLink>
-                                </MDBNavbarItem>
-                                <MDBNavbarItem>
-                                    <MDBNavbarLink href="/about">About</MDBNavbarLink>
-                                </MDBNavbarItem>
-                            </MDBNavbarNav>
-                        </div>
-
-                        {/* <div style={{marginLeft:'30vw'}}>
-                        </div> */}
-
-                        <div id="userVisit" style={userVisitStyle}>
-                        {/* <div className='d-md-flex align-items-center'  id="userVisit" style={userVisitStyle}> */}
-
-                            <MDBBtn href='/login' rounded color="info" size='sm' type='button' className='ms-md-2'>
-                                Login
-                            </MDBBtn>
-                            <MDBBtn href='/register' outline rounded color="secondary" size="sm" type='button' className='m-1'>
-                                Register
-                            </MDBBtn>
-
-                            <MDBNavbarLink href='#' className='d-flex flex-row align-items-center m-2'>
-                                <MDBBadge pill color='danger'>17</MDBBadge>
-                                <MDBIcon fas icon='shopping-cart' size='lg'></MDBIcon>
-                            </MDBNavbarLink>
-
-                        </div>
-
-                    </div>
-                </MDBCollapse>
-            </MDBContainer>
-        </MDBNavbar>
-    );
-}
+export default NavbarApp;
--- src/components/Navbar/NavbarLG.jsx
+++ src/components/Navbar/NavbarLG.jsx
@@ -7,7 +7,7 @@ import {
     MDBNavbarItem,
     MDBNavbarLink,
     MDBBtn,
-    MDBBadge,
+    MDBBadge
 } from 'mdb-react-ui-kit';
 import logo from '../../assets/images/logos/shopping-cart-logo.png';
 import { useEffect, useState } from 'react';
@@ -40,7 +40,7 @@ export default function NavbarLG() {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                gap: '.5rem',
+                // gap: '.5rem',
             });
             setUserCartStyle({ display: 'flex' });
         }
@@ -52,56 +52,51 @@ export default function NavbarLG() {
 
             <MDBContainer>
 
+            </MDBContainer>
                 <MDBNavbarBrand href="/">
                     <img src={logo} height="40" alt="" loading="lazy" />
                 </MDBNavbarBrand>
 
-                <MDBNavbarNav className="d-flex justify-content-between align-items-center">
 
-                    <MDBNavbarItem>
-                        <MDBNavbarLink active href="/products">Products</MDBNavbarLink>
-                    </MDBNavbarItem>
-                    <MDBNavbarItem>
-                        <MDBNavbarLink href="/contactus">Contact Us</MDBNavbarLink>
-                    </MDBNavbarItem>
-                    <MDBNavbarItem>
-                        <MDBNavbarLink href="/about">About</MDBNavbarLink>
-                    </MDBNavbarItem>
+                {/* <MDBCollapse navbar open={openBasic}> */}
+                    {/* <div className={Styles.collapse}> */}
+                    <div className='d-md-flex align-items-center'>
+                        <div>
+                            <MDBNavbarNav>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink active href="/products">Products</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href="/contactus">Contact Us</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href="/about">About</MDBNavbarLink>
+                                </MDBNavbarItem>
+                            </MDBNavbarNav>
+                        </div>
 
-                    <MDBNavbarItem id="userWelcome" style={userWelcomeStyle}>
-                        <MDBNavbarLink href="#">Username</MDBNavbarLink>
-                    </MDBNavbarItem>
+                        {/* <div style={{marginLeft:'30vw'}}>
+                        </div> */}
 
+                        <div id="userVisit" style={userVisitStyle}>
+                        {/* <div className='d-md-flex align-items-center'  id="userVisit" style={userVisitStyle}> */}
 
-                    <MDBNavbarItem>
-                        <MDBBtn className="bg-succes" type="button" size="sm" href="/login">
-                            Login <MDBIcon fas icon="sign-in-alt" />
-                        </MDBBtn>
+                            <MDBBtn href='/login' rounded color="info" size='sm' type='button' className='ms-md-2'>
+                                Login
+                            </MDBBtn>
+                            <MDBBtn href='/register' outline rounded color="secondary" size="sm" type='button' className='m-1'>
+                                Register
+                            </MDBBtn>
 
-                        <MDBBtn className="bg-info" type="button" size="sm" href="/register">
-                            Register <MDBIcon fas icon="registered" />
-                        </MDBBtn>
-                    </MDBNavbarItem>
+                            <MDBNavbarLink href='#' className='d-flex flex-row align-items-center m-2'>
+                                <MDBBadge pill color='danger'>17</MDBBadge>
+                                <MDBIcon fas icon='shopping-cart' size='lg'></MDBIcon>
+                            </MDBNavbarLink>
 
+                        </div>
 
-                    <MDBNavbarItem id="userCart" style={userCartStyle} className='d-flex align-items-center'>
-
-                        <MDBBadge
-                            color="danger"
-                            style={{ fontSize: "1rem", width: '100%', height: '2rem', borderRadius: '50%' }}
-                        >
-                            <p className='mt-1'>{cartItemsCount}</p>
-
-                        </MDBBadge>
-
-                        <MDBNavbarLink href='/cart'>
-                            <MDBIcon fas icon="shopping-cart" size='2x' />
-                        </MDBNavbarLink>
-
-                    </MDBNavbarItem>
-
-                </MDBNavbarNav>
-            </MDBContainer>
+                    </div>
+                {/* </MDBCollapse> */}
         </MDBNavbar>
     );
 }
