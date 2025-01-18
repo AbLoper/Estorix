a37eb3c2c760d32572c368ab38d6b8badbab7c6b
AbLoper <abedslayman1982@gmail.com>
2025-01-18 11:05:34 +0200

    Update Code

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import routes from './routes';
 
-// استيراد مكون الـ Navbar
-import Navbar from './components/navbar/Navbar.jsx'
+import Navbar from './components/navbar/Navbar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-      {/* إضافة الـ Navbar هنا ليظهر فوق جميع الصفحات */}
       <Navbar />
-
       <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
