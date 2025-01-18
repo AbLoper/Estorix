62288353cd79a8c1fffe85f9bcb21e995b3cb72f
AbLoper <abedslayman1982@gmail.com>
2025-01-18 09:50:18 +0200

    Update Code

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ import store from './reduxToolkit/store';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import routes from './routes';
+import NavbarApp from './components/navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <NavbarApp/>
     <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
         ))}
       </Routes>
-
     </BrowserRouter>
   </Provider>
 );
