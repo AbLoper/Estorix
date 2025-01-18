795212d1abb64de32d650f31a1280ca9c46bcc9b
AbLoper <abedslayman1982@gmail.com>
2025-01-18 11:00:16 +0200

    Update Code

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import store from './reduxToolkit/store';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import routes from './routes';
-import NavbarApp from './components/navbar/Navbar';
+
+// استيراد مكون الـ Navbar
+import Navbar from './components/navbar/Navbar.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <NavbarApp/>
     <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+      {/* إضافة الـ Navbar هنا ليظهر فوق جميع الصفحات */}
+      <Navbar />
+
       <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
