bdc4565109602437506ea0f018ee208e5e8a2be3
AbLoper <abedslayman1982@gmail.com>
2025-01-22 19:06:23 +0200

    Update Code

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -47,7 +47,7 @@ export default function Product() {
                 style={{
                   objectFit: 'contain',
                   // width: '100%',
-                  height:'85vh',
+                  // height:'70vh',
                   padding: '1rem'
                 }}
               />
